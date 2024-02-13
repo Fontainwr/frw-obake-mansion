00cb9f264b54d0611f0258c404e4ac8f47ce6fff
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-13 14:29:11 +0900

    Added NAvBar
    Working on the Navbar now.

--- src/compoponets/Navbar/Navbar.jsx
+++ src/compoponets/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 import styles from "./Navbar.module.css"
+import { getImageUrl } from "../../utils";
+
 
 export const Navbar = () => {
     return (
     <nav className={styles.navbar}>
         <a className={styles.title}  href="/">Portfolio</a>
         <div className={styles.menu}>
-            <ul className={styles.menuitem}>
+        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu-button"/>
+            <ul className={styles.menuItems}>
                 <li>
                     <a href="#about">About</a>
                 </li>
