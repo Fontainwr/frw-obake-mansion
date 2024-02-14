91087985668ab5aaaf861b885b2b0947c8e65bbb
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-14 11:56:43 +0900

    Main
    Working on main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
+import Hero from "./compoponets/Hero/Hero";
 import { Navbar } from "./compoponets/Navbar/Navbar";
 
 function App() {
   return ( 
   <div className={styles.App}>
     <Navbar />
+    <Hero />
   </div>
   );
 }
---
+++ src/compoponets/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+    return (
+            <section className={styles.container}> 
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I'm Miyuki Richardson </h1>
+                    <p className={styles.description}>
+                    I own the Obake Mansion company. Please reach out to the company
+                    </p>
+                    <a href="mailto:maruyama.miyuki@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            </div>
+                <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} />
+                    <div className={styles.topBlur} />
+                    <div className={styles.bottomBlur}></div>
+    </section>
+    );
+};
+
+export default Hero
