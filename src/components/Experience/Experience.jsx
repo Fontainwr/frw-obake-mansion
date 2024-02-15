b09e19f291c7f816c37fa065eb25b086ca41bafd
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-15 14:06:30 +0900

    Added pics
    Added some more info for the site.

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 
 import styles from "./Experience.module.css";
-import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
     return (
         <section className={styles.container} id="experience">
-        <h2 className={styles.title}>Works</h2>
-        <div className={styles.content}>
-            <div className={styles.skills}>
-            {skills.map((skill, id) => {
-                return (
-                <div key={id} className={styles.skill}>
-                    <div className={styles.skillImageContainer}>
-                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                    </div>
-                    <p>{skill.title}</p>
-                </div>
-                );
-            })}
-            </div>
+        <h2 className={styles.title}>Partners & Companies</h2>
             <ul className={styles.history}>
             {history.map((historyItem, id) => {
                 return (
@@ -43,7 +29,6 @@ export const Experience = () => {
                 );
             })}
             </ul>
-        </div>
         </section>
     );
 };
