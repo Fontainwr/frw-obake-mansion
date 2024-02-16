be549faa90e00df7d7f6992e8055ae132dc315d5
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-16 15:37:30 +0900

    done
    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
-import{ Hero } from "./components/Hero/Hero";
-import { Navbar } from "./components/Navbar/Navbar";
+import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
 
 function App() {
-  return ( 
-  <div className={styles.App}>
-    <Navbar />
-    <Hero />
-    <About />
-    <Experience />
-    <Projects />
-  </div>
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
